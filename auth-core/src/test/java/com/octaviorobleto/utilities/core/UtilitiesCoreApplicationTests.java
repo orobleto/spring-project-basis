74518ebed6ebd6c773be1761aa74751f4b1d924b
Octavio Robleto <octavio.robleto@gmail.com>
2023-06-15 12:31:57 -0400

    Se agregan los proyectos basicos (utilidades generales, la logica de autenticacion y exposicion de endpoint)

---
+++ auth-core/src/test/java/com/octaviorobleto/utilities/core/UtilitiesCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octaviorobleto.utilities.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitiesCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
